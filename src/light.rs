a878000ec54343fd21d816772b11fb139ff916f6
Alexi Chepura <a.chepura@gmail.com>
2022-07-07 14:01:23 +0300

    lights

--- src/light.rs
+++ src/light.rs
@@ -15,28 +15,28 @@ pub fn light_start_system(mut commands: Commands) {
     });
 
     // directional 'sun' light
-    // const HALF_SIZE: f32 = 10.0;
-    // commands.spawn_bundle(DirectionalLightBundle {
-    //     directional_light: DirectionalLight {
-    //         illuminance: 5000.0,
-    //         // Configure the projection to better fit the scene
-    //         shadow_projection: OrthographicProjection {
-    //             left: -HALF_SIZE,
-    //             right: HALF_SIZE,
-    //             bottom: -HALF_SIZE,
-    //             top: HALF_SIZE,
-    //             near: -10.0 * HALF_SIZE,
-    //             far: 10.0 * HALF_SIZE,
-    //             ..default()
-    //         },
-    //         shadows_enabled: true,
-    //         ..default()
-    //     },
-    //     transform: Transform {
-    //         translation: Vec3::new(0.0, 2.0, 0.0),
-    //         rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_4),
-    //         ..default()
-    //     },
-    //     ..default()
-    // });
+    const HALF_SIZE: f32 = 10.0;
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 5000.0,
+            // Configure the projection to better fit the scene
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 10.0 * HALF_SIZE,
+                ..default()
+            },
+            // shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_4),
+            ..default()
+        },
+        ..default()
+    });
 }
