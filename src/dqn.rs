e38090682133151f8f040cdf481da37c7f8a56fd
Alexi Chepura <a.chepura@gmail.com>
2022-08-07 09:50:38 +0300

    epsilon

--- src/dqn.rs
+++ src/dqn.rs
@@ -1,6 +1,12 @@
 use bevy::prelude::*;
 use dfdx::prelude::*;
+use rand::Rng;
 use std::time::Instant;
+
+// https://github.com/coreylowman/dfdx/blob/main/examples/dqn.rs
+// https://github.com/mswang12/minDQN/blob/main/minDQN.py
+// https://iq.opengenus.org/deep-q-learning/
+// https://towardsdatascience.com/deep-q-learning-tutorial-mindqn-2a4c855abffc
 
 const SENSORS_SIZE: usize = 7;
 const STATE_SIZE: usize = SENSORS_SIZE + 2;
@@ -21,6 +27,10 @@ pub struct DqnResource {
     pub action: [usize; 64],
     pub reward: Tensor1D<64>,
     pub done: Tensor1D<64>,
+    pub epsilon: f32,
+    pub max_epsilon: f32,
+    pub min_epsilon: f32,
+    pub decay: f32,
 }
 impl DqnResource {
     pub fn default() -> Self {
@@ -33,6 +43,10 @@ impl DqnResource {
             action: [(); 64].map(|_| 0),
             reward: Tensor1D::zeros(),
             done: Tensor1D::zeros(),
+            epsilon: 1.,
+            max_epsilon: 1.,
+            min_epsilon: 0.01,
+            decay: 0.01,
         }
     }
 }
@@ -45,18 +59,33 @@ pub fn dqn_start_system(world: &mut World) {
         }),
     });
 }
-pub fn dqn_system(mut dqn: NonSendMut<DqnResource>, mut sgd: NonSendMut<SgdResource>) {
-    let start = Instant::now();
-    // targ_q = R + discount * max(Q(S'))
-    // curr_q = Q(S)[A]
-    // loss = mse(curr_q, targ_q)
-    let next_q_values = dqn.tqn.forward(dqn.next_state.clone());
-    let max_next_q = next_q_values.max_last_dim();
-    let target_q = 0.99 * mul(max_next_q, &(1.0 - dqn.done.clone())) + &dqn.reward;
-    let q_values = dqn.qn.forward(dqn.state.trace());
-    let loss = mse_loss(q_values.gather_last_dim(&dqn.action), &target_q);
-    let loss_v = *loss.data();
-    let gradients = loss.backward();
-    sgd.sgd.update(&mut dqn.qn, gradients);
-    println!("q loss={:#.3} in {:?}", loss_v, start.elapsed());
+
+pub fn dqn_system(
+    time: Res<Time>,
+    mut dqn: NonSendMut<DqnResource>,
+    mut sgd: NonSendMut<SgdResource>,
+) {
+    let seconds = time.seconds_since_startup();
+    let mut rng = rand::thread_rng();
+    let random_number = rng.gen_range(0.0..1.0);
+    if random_number <= dqn.epsilon {
+        // action = env.action_space.sample()
+    } else {
+        let start = Instant::now();
+        // targ_q = R + discount * max(Q(S'))
+        // curr_q = Q(S)[A]
+        // loss = mse(curr_q, targ_q)
+        let next_q_values = dqn.tqn.forward(dqn.next_state.clone());
+        let max_next_q = next_q_values.max_last_dim();
+        let discount = 0.99;
+        let target_q = discount * mul(max_next_q, &(1.0 - dqn.done.clone())) + &dqn.reward;
+        let q_values = dqn.qn.forward(dqn.state.trace());
+        let loss = mse_loss(q_values.gather_last_dim(&dqn.action), &target_q);
+        let loss_v = *loss.data();
+        let gradients = loss.backward();
+        sgd.sgd.update(&mut dqn.qn, gradients);
+        println!("q loss={:#.3} in {:?}", loss_v, start.elapsed());
+    }
+    dqn.epsilon =
+        dqn.min_epsilon + (dqn.max_epsilon - dqn.min_epsilon) * (-dqn.decay * seconds as f32);
 }
