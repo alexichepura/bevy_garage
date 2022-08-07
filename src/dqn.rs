daec27b75912d1826736d8d9b6182845d2e1c146
Alexi Chepura <a.chepura@gmail.com>
2022-08-07 10:28:50 +0300

    replay, WIP

--- src/dqn.rs
+++ src/dqn.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 use dfdx::prelude::*;
-use rand::Rng;
+use rand::{rngs::StdRng, Rng, SeedableRng};
 use std::time::Instant;
 
 // https://github.com/coreylowman/dfdx/blob/main/examples/dqn.rs
@@ -16,17 +16,41 @@ type QNetwork = (
     (Linear<32, 32>, ReLU),
     Linear<32, ACTION_SIZE>,
 );
-pub struct SgdResource {
-    pub sgd: Sgd<QNetwork>,
-}
-pub struct DqnResource {
-    pub qn: QNetwork,
-    pub tqn: QNetwork,
-    pub state: Tensor2D<64, STATE_SIZE>,
-    pub next_state: Tensor2D<64, STATE_SIZE>,
+type TState = Tensor2D<64, STATE_SIZE>;
+pub struct ReplayMemory {
+    pub state: TState,
+    pub next_state: TState,
     pub action: [usize; 64],
     pub reward: Tensor1D<64>,
     pub done: Tensor1D<64>,
+}
+impl ReplayMemory {
+    pub fn default() -> Self {
+        let mut rng = StdRng::seed_from_u64(0);
+        Self {
+            // state: Tensor2D::zeros(),
+            state: Tensor2D::randn(&mut rng),
+            // next_state: Tensor2D::zeros(),
+            next_state: Tensor2D::randn(&mut rng),
+            // action: [(); 64].map(|_| 0),
+            action: [(); 64].map(|_| rng.gen_range(0..ACTION_SIZE)),
+            // reward: Tensor1D::zeros(),
+            reward: Tensor1D::randn(&mut rng),
+            // done: Tensor1D::zeros(),
+            done: Tensor1D::zeros(),
+        }
+    }
+}
+pub struct DqnResource {
+    pub seconds: i32,
+    pub qn: QNetwork,
+    pub tqn: QNetwork,
+    pub replay: ReplayMemory,
+    // pub state: TState,
+    // pub next_state: TState,
+    // pub action: [usize; 64],
+    // pub reward: Tensor1D<64>,
+    // pub done: Tensor1D<64>,
     pub epsilon: f32,
     pub max_epsilon: f32,
     pub min_epsilon: f32,
@@ -36,19 +60,19 @@ impl DqnResource {
     pub fn default() -> Self {
         let qn: QNetwork = Default::default();
         Self {
+            seconds: 0,
             qn: qn.clone(),
             tqn: qn.clone(),
-            state: Tensor2D::zeros(),
-            next_state: Tensor2D::zeros(),
-            action: [(); 64].map(|_| 0),
-            reward: Tensor1D::zeros(),
-            done: Tensor1D::zeros(),
+            replay: ReplayMemory::default(),
             epsilon: 1.,
             max_epsilon: 1.,
             min_epsilon: 0.01,
             decay: 0.01,
         }
     }
+}
+pub struct SgdResource {
+    pub sgd: Sgd<QNetwork>,
 }
 pub fn dqn_start_system(world: &mut World) {
     world.insert_non_send_resource(DqnResource::default());
@@ -70,21 +94,28 @@ pub fn dqn_system(
     let random_number = rng.gen_range(0.0..1.0);
     if random_number <= dqn.epsilon {
         // action = env.action_space.sample()
+        println!("TODO random action");
     } else {
         let start = Instant::now();
         // targ_q = R + discount * max(Q(S'))
         // curr_q = Q(S)[A]
         // loss = mse(curr_q, targ_q)
-        let next_q_values = dqn.tqn.forward(dqn.next_state.clone());
+        let next_q_values = dqn.tqn.forward(dqn.replay.next_state.clone());
         let max_next_q = next_q_values.max_last_dim();
         let discount = 0.99;
-        let target_q = discount * mul(max_next_q, &(1.0 - dqn.done.clone())) + &dqn.reward;
-        let q_values = dqn.qn.forward(dqn.state.trace());
-        let loss = mse_loss(q_values.gather_last_dim(&dqn.action), &target_q);
+        let target_q =
+            discount * mul(max_next_q, &(1.0 - dqn.replay.done.clone())) + &dqn.replay.reward;
+        let q_values = dqn.qn.forward(dqn.replay.state.trace());
+        let loss = mse_loss(q_values.gather_last_dim(&dqn.replay.action), &target_q);
         let loss_v = *loss.data();
         let gradients = loss.backward();
         sgd.sgd.update(&mut dqn.qn, gradients);
-        println!("q loss={:#.3} in {:?}", loss_v, start.elapsed());
+        let seconds_round = seconds.round() as i32;
+        if seconds_round > dqn.seconds {
+            println!("q loss={:#.3} in {:?}", loss_v, start.elapsed());
+            dbg!(dqn.replay.action);
+            dqn.seconds = seconds_round + 1;
+        }
     }
     dqn.epsilon =
         dqn.min_epsilon + (dqn.max_epsilon - dqn.min_epsilon) * (-dqn.decay * seconds as f32);
