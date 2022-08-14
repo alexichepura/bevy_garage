4e8b3dc5921f72aa627da1c110e35a9d71db6185
Alexi Chepura <a.chepura@gmail.com>
2022-08-14 09:05:27 +0300

    rb

--- src/dqn.rs
+++ src/dqn.rs
@@ -14,6 +14,7 @@ use crate::{car::Car, progress::CarProgress};
 // https://towardsdatascience.com/deep-q-learning-tutorial-mindqn-2a4c855abffc
 
 const SIZE: usize = 64;
+const BUFFER_SIZE: usize = 50_000;
 const SENSORS_SIZE: usize = 7;
 const STATE_SIZE: usize = SENSORS_SIZE + 2;
 const ACTION_SIZE: usize = 4;
@@ -22,78 +23,68 @@ type QNetwork = (
     (Linear<32, 32>, ReLU),
     Linear<32, ACTION_SIZE>,
 );
-type StateSizeTensor = Tensor1D<STATE_SIZE>;
-type ActionSizeTensor = Tensor1D<ACTION_SIZE>;
-type ObsState = [f32; STATE_SIZE];
+type State1D = Tensor1D<STATE_SIZE>;
+type Action1D = Tensor1D<ACTION_SIZE>;
+type Observation = [f32; STATE_SIZE];
 
-pub struct ReplayMemory {
-    pub state: StateSizeTensor,
-    pub next_state: StateSizeTensor,
-    pub action: [usize; SIZE],
-    pub reward: Tensor1D<SIZE>,
-    pub done: Tensor0D,
+pub struct ReplayBuffer {
+    pub state: [Observation; BUFFER_SIZE],
+    pub action: [usize; BUFFER_SIZE],
+    pub reward: [f32; BUFFER_SIZE],
+    pub next_state: [Observation; BUFFER_SIZE],
     pub i: usize,
 }
-impl ReplayMemory {
-    pub fn default() -> Self {
+impl ReplayBuffer {
+    pub fn new() -> Self {
         Self {
-            state: Tensor1D::zeros(),
-            next_state: Tensor1D::zeros(),
-            action: [(); SIZE].map(|_| 0),
-            reward: Tensor1D::zeros(),
-            done: Tensor0D::zeros(),
+            state: [[0.; STATE_SIZE]; BUFFER_SIZE],
+            action: [0; BUFFER_SIZE],
+            reward: [0.; BUFFER_SIZE],
+            next_state: [[0.; STATE_SIZE]; BUFFER_SIZE],
             i: 0,
         }
     }
-    pub fn random() -> Self {
-        let mut rng = StdRng::seed_from_u64(0);
-        Self {
-            state: Tensor1D::randn(&mut rng),
-            next_state: Tensor1D::randn(&mut rng),
-            action: [(); SIZE].map(|_| rng.gen_range(0..ACTION_SIZE)),
-            reward: Tensor1D::randn(&mut rng),
-            done: Tensor0D::zeros(),
-            i: 0,
-        }
+    pub fn store(
+        &mut self,
+        state: Observation,
+        action: usize,
+        reward: f32,
+        next_state: Observation,
+    ) {
+        let i = self.i % SIZE;
+        self.state[i] = state;
+        self.action[i] = action;
+        self.reward[i] = reward;
+        self.next_state[i] = next_state;
+        self.i += 1;
     }
-    // pub fn store(
-    //     &mut self,
-    //     state: TSingleState,
-    //     next_state: TSingleState,
-    //     action: usize,
-    //     reward: f32,
-    // ) {
-    //     let i = self.i % SIZE;
-    //     self.state.mut_data()[i] = state;
-    //     self.next_state.mut_data()[i] = next_state;
-    //     self.action[i] = action;
-    //     self.reward.mut_data()[i] = reward;
-    //     self.i += 1;
-    // }
 }
 pub struct DqnResource {
     pub seconds: i32,
     pub qn: QNetwork,
     pub tqn: QNetwork,
-    pub rpl: ReplayMemory,
+    pub rpl: ReplayBuffer,
     pub epsilon: f32,
     pub max_epsilon: f32,
     pub min_epsilon: f32,
     pub decay: f32,
+    pub done: f32,
 }
 impl DqnResource {
     pub fn default() -> Self {
-        let qn: QNetwork = Default::default();
+        let mut rng = StdRng::seed_from_u64(0);
+        let mut qn: QNetwork = Default::default();
+        qn.reset_params(&mut rng);
         Self {
             seconds: 0,
             qn: qn.clone(),
             tqn: qn.clone(),
-            rpl: ReplayMemory::random(),
-            // rpl: ReplayMemory::default(),
+            rpl: ReplayBuffer::new(),
             epsilon: 1.,
             max_epsilon: 1.,
             min_epsilon: 0.01,
             decay: 0.01,
+            done: 0.,
         }
     }
 }
@@ -117,7 +108,7 @@ pub fn dqn_system(
     q_car: Query<(&Car, &Velocity, &CarProgress), With<Car>>,
 ) {
     let (car, v, progress) = q_car.single();
-    let obs_state: ObsState = [
+    let obs: Observation = [
         car.sensor_inputs[0],
         car.sensor_inputs[1],
         car.sensor_inputs[2],
@@ -128,7 +119,7 @@ pub fn dqn_system(
         v.linvel.length(),
         progress.meters,
     ];
-    let obs_state_tensor = Tensor1D::new(obs_state);
+    let obs_state_tensor = Tensor1D::new(obs);
 
     let seconds = time.seconds_since_startup();
     let mut rng = rand::thread_rng();
@@ -139,36 +130,38 @@ pub fn dqn_system(
         action = rng.gen_range(0..3);
     } else {
         let start = Instant::now();
-        let q_values: ActionSizeTensor = dqn.tqn.forward(obs_state_tensor);
-        dbg!(q_values.data());
+        let next_q_values: Action1D = dqn.tqn.forward(obs_state_tensor.clone());
+        let max_next_q_value = *next_q_values.clone().max_last_dim().data();
+        action = next_q_values
+            .clone()
+            .data()
+            .iter()
+            .position(|q| *q == max_next_q_value)
+            .unwrap();
+        // let max_next_q = mul(max_next_q_values.clone(), &(1.0 - dqn.done));
+        let reward = progress.meters;
+        let prev_state = obs; // TODO !!!!!!!!!!!
+        dqn.rpl.store(prev_state, action, reward, obs);
+
         // targ_q = R + discount * max(Q(S'))
-        let max_next_q_values = q_values.max_last_dim();
-        // dbg!(max_next_q_values.data());
+        // curr_q = Q(S)[A]
+        // loss = mse(curr_q, targ_q)
+        let target_q = 0.99 * max_next_q_value + reward;
+        let target_q_tensor = Tensor0D::new(target_q);
+        let q_values = dqn.qn.forward(obs_state_tensor.trace());
+        let curr_q = q_values.gather_last_dim(&action);
+        let loss = mse_loss(curr_q, &target_q_tensor);
+        let loss_v = *loss.data();
 
-        // let max_next_q = mul(max_next_q_values, &(1.0 - dqn.rpl.done.clone()));
-        // let discount = 0.99;
-        // let reward = 1.; // &dqn.rpl.reward;
-        // let target_q = discount * max_next_q + reward;
-        // let max_target_q = target_q.clone().max_last_dim();
-
-        // // curr_q = Q(S)[A]
-        // let q_values = dqn.qn.forward(dqn.rpl.state.trace());
-        // let curr_q = q_values.gather_last_dim(&dqn.rpl.action);
-
-        // // loss = mse(curr_q, targ_q)
-        // let loss = mse_loss(curr_q, &target_q);
-        // let loss_v = *loss.data();
-
-        // let gradients = loss.backward();
-        // sgd.sgd.update(&mut dqn.qn, gradients);
-        // let seconds_round = seconds.round() as i32;
-        // if seconds_round > dqn.seconds {
-        //     println!("q loss={:#.3} in {:?}", loss_v, start.elapsed());
-        //     dbg!(dqn.rpl.action);
-        //     dqn.seconds = seconds_round + 1;
-        // }
+        let gradients = loss.backward();
+        sgd.sgd.update(&mut dqn.qn, gradients);
+        let seconds_round = seconds.round() as i32;
+        if seconds_round > dqn.seconds {
+            println!("q loss={:#.3} in {:?}", loss_v, start.elapsed());
+            dbg!(dqn.rpl.action);
+            dqn.seconds = seconds_round + 1;
+        }
     }
-    // dqn.rpl.store(obs_state, obs_state, action, 0.);
     dqn.epsilon =
         dqn.min_epsilon + (dqn.max_epsilon - dqn.min_epsilon) * (-dqn.decay * seconds as f32);
 }
