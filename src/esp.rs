50a2a42be2be7041c983e957681f1039542082f3
Alexi Chepura <a.chepura@gmail.com>
2022-08-22 11:21:28 +0300

    tweaks

--- src/esp.rs
+++ src/esp.rs
@@ -32,7 +32,7 @@ pub fn esp_system(
         let car_kmh = car_mps / 1000. * 3600.;
         let torque_speed_x: f32 = match braking {
             true => 3.,
-            _ => match car_kmh / 150. {
+            _ => match car_kmh / 120. {
                 x if x >= 1. => 0.,
                 x => 1. - x,
             },
--- src/track.rs
+++ src/track.rs
@@ -106,7 +106,7 @@ pub fn track_start_system(
     // let num_cols: usize = 8;
     let num_rows: usize = 2;
     let num_cols: usize = 2;
-    let scale = 500.;
+    let scale = 600.;
     let hx = num_cols as f32 * scale;
     let hy = 0.5;
     let hz = num_rows as f32 * scale;
