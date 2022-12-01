3adbe5263c94aabf163f3336086b9d0d31b989e0
Alexi Chepura <a.chepura@gmail.com>
2022-12-01 12:04:51 +0300

    more logic to track

--- src/track.rs
+++ src/track.rs
@@ -24,6 +24,8 @@ pub struct Track {
     dirs: Vec<Vec3>,
     left: Vec<Vec3>,
     right: Vec<Vec3>,
+    left_norm: Vec<Vec3>,
+    right_norm: Vec<Vec3>,
 }
 
 impl Track {
@@ -37,6 +39,8 @@ impl Track {
             dirs: Vec::new(),
             left: Vec::new(),
             right: Vec::new(),
+            left_norm: Vec::new(),
+            right_norm: Vec::new(),
         }
     }
     pub fn new() -> Self {
@@ -70,12 +74,13 @@ impl Track {
             track.collider_indices.push(dindices[0]);
             track.collider_indices.push(dindices[1]);
             track.dirs.push(dir);
-            track
-                .left
-                .push(Quat::from_rotation_y(FRAC_PI_2).mul_vec3(dir));
-            track
-                .right
-                .push(Quat::from_rotation_y(-FRAC_PI_2).mul_vec3(dir));
+
+            let left_norm = Quat::from_rotation_y(FRAC_PI_2).mul_vec3(dir);
+            let right_norm = Quat::from_rotation_y(-FRAC_PI_2).mul_vec3(dir);
+            track.left_norm.push(left_norm);
+            track.right_norm.push(right_norm);
+            track.left.push(*point + left_norm * track.width);
+            track.right.push(*point + right_norm * track.width);
         }
 
         track
@@ -83,13 +88,9 @@ impl Track {
     pub fn road(&self) -> (Vec<[f32; 3]>, Vec<[f32; 3]>) {
         let mut vertices: Vec<[f32; 3]> = vec![];
         let mut normals: Vec<[f32; 3]> = vec![];
-        for (i, point) in self.points.iter().enumerate() {
-            let sides: (Vec3, Vec3) = (
-                *point + self.left[i] * self.width,
-                *point + self.right[i] * self.width,
-            );
-            vertices.push(sides.0.into());
-            vertices.push(sides.1.into());
+        for (i, _) in self.points.iter().enumerate() {
+            vertices.push(self.left[i].into());
+            vertices.push(self.right[i].into());
             normals.push(Vec3::Y.into());
             normals.push(Vec3::Y.into());
         }
@@ -101,7 +102,7 @@ pub fn spawn_road(
     commands: &mut Commands,
     meshes: &mut ResMut<Assets<Mesh>>,
     materials: &mut ResMut<Assets<StandardMaterial>>,
-    track: Track,
+    track: &Track,
 ) {
     let (vertices, normals) = track.road();
 
@@ -111,7 +112,7 @@ pub fn spawn_road(
         VertexAttributeValues::from(vertices.clone()),
     );
     mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, VertexAttributeValues::from(normals));
-    mesh.set_indices(Some(Indices::U32(track.indices)));
+    mesh.set_indices(Some(Indices::U32(track.indices.clone())));
 
     commands
         .spawn(PbrBundle {
@@ -126,7 +127,7 @@ pub fn spawn_road(
                 .iter()
                 .map(|v| Point3::new(v[0], v[1], v[2]))
                 .collect(),
-            track.collider_indices,
+            track.collider_indices.clone(),
         )))
         .insert(ColliderScale::Absolute(Vec3::ONE))
         .insert(CollisionGroups::new(STATIC_GROUP, Group::ALL))
@@ -144,68 +145,22 @@ pub fn track_start_system(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let track = Track::new();
-
     let mut left_wall_vertices: Vec<[f32; 3]> = vec![];
-    let mut left_wall_indices: Vec<u32> = vec![];
-    let mut left_wall_collider_indices: Vec<[u32; 3]> = Vec::new();
-    // let mut left_wall_normals: Vec<[f32; 3]> = vec![];
     let mut right_wall_vertices: Vec<[f32; 3]> = vec![];
-    let mut right_wall_indices: Vec<u32> = vec![];
-    let mut right_wall_collider_indices: Vec<[u32; 3]> = Vec::new();
-    // let mut right_wall_normals: Vec<[f32; 3]> = vec![];
-
-    let width: f32 = 5.;
-    for (i, point) in track.points.iter().enumerate() {
-        let last: bool = i + 1 == track.points.len();
-        let ind: u32 = i as u32 * 2;
-        let ituple: [[u32; 3]; 2] = [
-            [ind, ind + 1, if last { 0 } else { ind + 2 }],
-            [
-                if last { 0 } else { ind + 2 },
-                ind + 1,
-                if last { 1 } else { ind + 3 },
-            ],
-        ];
-        let i_next: usize = if last { 0 } else { i + 1 };
-        let point_next = track.points.get(i_next).unwrap();
-        let point_direction: Vec3 = (*point_next - *point).normalize();
-        let dv: (Vec3, Vec3) = (
-            Quat::from_rotation_y(FRAC_PI_2).mul_vec3(point_direction) * width,
-            Quat::from_rotation_y(-FRAC_PI_2).mul_vec3(point_direction) * width,
-        );
-        let sides: (Vec3, Vec3) = (*point + dv.0, *point + dv.1);
-
-        let walls: (Vec3, Vec3) = (sides.0 + Vec3::Y, sides.1 + Vec3::Y);
-        left_wall_vertices.push(sides.0.into());
-        left_wall_vertices.push(walls.0.into());
-        left_wall_indices.extend(ituple[0]);
-        left_wall_indices.extend(ituple[1]);
-        left_wall_collider_indices.push(ituple[0]);
-        left_wall_collider_indices.push(ituple[1]);
-        // left_wall_normals.push(normal.clone());
-        // left_wall_normals.push(normal.clone());
-
-        right_wall_vertices.push(sides.1.into());
-        right_wall_vertices.push(walls.1.into());
-        right_wall_indices.extend(ituple[0]);
-        right_wall_indices.extend(ituple[1]);
-        right_wall_collider_indices.push(ituple[0]);
-        right_wall_collider_indices.push(ituple[1]);
-        // right_wall_normals.push(normal.clone());
-        // right_wall_normals.push(normal.clone());
+    for (i, _) in track.points.iter().enumerate() {
+        left_wall_vertices.push(track.left[i].into());
+        left_wall_vertices.push((track.left[i] + Vec3::Y).into());
+        right_wall_vertices.push(track.right[i].into());
+        right_wall_vertices.push((track.right[i] + Vec3::Y).into());
     }
-    spawn_road(&mut commands, &mut meshes, &mut materials, track);
+    spawn_road(&mut commands, &mut meshes, &mut materials, &track);
 
     let mut left_wall_mesh = Mesh::new(PrimitiveTopology::TriangleList);
     left_wall_mesh.insert_attribute(
         Mesh::ATTRIBUTE_POSITION,
         VertexAttributeValues::from(left_wall_vertices.clone()),
     );
-    // left_wall_mesh.insert_attribute(
-    //     Mesh::ATTRIBUTE_NORMAL,
-    //     VertexAttributeValues::from(road_normals),
-    // );
-    left_wall_mesh.set_indices(Some(Indices::U32(left_wall_indices)));
+    left_wall_mesh.set_indices(Some(Indices::U32(track.indices.clone())));
     commands
         .spawn(PbrBundle {
             mesh: meshes.add(left_wall_mesh),
@@ -218,7 +173,7 @@ pub fn track_start_system(
                 .iter()
                 .map(|v| Point3::new(v[0], v[1], v[2]))
                 .collect(),
-            left_wall_collider_indices,
+            track.collider_indices.clone(),
         )))
         .insert(ColliderScale::Absolute(Vec3::ONE))
         .insert(CollisionGroups::new(STATIC_GROUP, Group::ALL))
@@ -234,7 +189,7 @@ pub fn track_start_system(
         Mesh::ATTRIBUTE_POSITION,
         VertexAttributeValues::from(right_wall_vertices.clone()),
     );
-    right_wall_mesh.set_indices(Some(Indices::U32(right_wall_indices)));
+    right_wall_mesh.set_indices(Some(Indices::U32(track.indices.clone())));
     commands
         .spawn(PbrBundle {
             mesh: meshes.add(right_wall_mesh),
@@ -247,7 +202,7 @@ pub fn track_start_system(
                 .iter()
                 .map(|v| Point3::new(v[0], v[1], v[2]))
                 .collect(),
-            right_wall_collider_indices,
+            track.collider_indices.clone(),
         )))
         .insert(ColliderScale::Absolute(Vec3::ONE))
         .insert(CollisionGroups::new(STATIC_GROUP, Group::ALL))
