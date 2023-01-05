02bb21267a82f7035b01a35b9d95f1994fb20a55
Alexi Chepura <a.chepura@gmail.com>
2023-01-05 16:20:35 +0300

    tighten rapier integration_parameters

--- src/lib.rs
+++ src/lib.rs
@@ -30,9 +30,11 @@ use progress::*;
 use track::*;
 
 fn rapier_config_start_system(mut c: ResMut<RapierContext>) {
-    c.integration_parameters.max_velocity_iterations = 256;
-    c.integration_parameters.max_velocity_friction_iterations = 64;
-    c.integration_parameters.max_stabilization_iterations = 256;
+    c.integration_parameters.max_velocity_iterations = 64;
+    c.integration_parameters.max_velocity_friction_iterations = 16;
+    c.integration_parameters.max_stabilization_iterations = 64;
+    c.integration_parameters.allowed_linear_error = 0.0001;
+    c.integration_parameters.erp = 0.99;
     dbg!(c.integration_parameters);
 }
 
