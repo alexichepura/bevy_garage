d9bba9b22faa634d8288948aa9f2fec8cda71631
Alexi Chepura <a.chepura@gmail.com>
2023-01-15 06:02:52 +0300

    features simulator

--- xr/src/lib.rs
+++ xr/src/lib.rs
@@ -6,7 +6,18 @@ use bevy::{
 use bevy_rapier_car_sim::car_app;
 
 pub fn game_main() {
+    std::env::set_var("RUST_BACKTRACE", "1");
+    std::env::set_var("RUST_LOG", "bevy_openxr=info");
     let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    // app.add_plugins(DefaultPlugins.set(WindowPlugin {
+    //     window: WindowDescriptor {
+    //         resizable: false,
+    //         mode: WindowMode::BorderlessFullscreen,
+    //         ..default()
+    //     },
+    //     ..default()
+    // }));
     car_app(&mut app);
     app.add_startup_system(xr_startup);
     app.run();
