783ed469093fe9fba61ae0f1c7945c586380ac42
Alexi Chepura <a.chepura@gmail.com>
2023-04-27 20:47:22 +0300

    main cleanup from SamplerDescriptor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bevy::{prelude::*, window::WindowResolution};
 use bevy_garage::{camera::CarCameraPlugin, car_app, PhysicsParams};
-use wgpu::{AddressMode, SamplerDescriptor};
 
 fn main() {
     let mut app = App::new();
@@ -8,27 +7,15 @@ fn main() {
     let res = WindowResolution::default();
     #[cfg(target_arch = "wasm32")]
     let res = WindowResolution::new(720., 360.);
-    app.add_plugins(
-        DefaultPlugins
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "Bevy Garage".to_string(),
-                    resolution: res,
-                    canvas: Some("#bevy-garage".to_string()),
-                    ..default()
-                }),
-                ..default()
-            })
-            .set(ImagePlugin {
-                default_sampler: SamplerDescriptor {
-                    address_mode_u: AddressMode::Repeat,
-                    address_mode_v: AddressMode::Repeat,
-                    address_mode_w: AddressMode::Repeat,
-                    ..Default::default()
-                },
-                ..default()
-            }),
-    );
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Bevy Garage".to_string(),
+            resolution: res,
+            canvas: Some("#bevy-garage".to_string()),
+            ..default()
+        }),
+        ..default()
+    }));
     app.add_plugin(CarCameraPlugin);
 
     #[cfg(target_arch = "wasm32")]
