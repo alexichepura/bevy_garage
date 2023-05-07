97165fc53b551d9f9f73e6bd5180e1392a5ccaac
Alexi Chepura <a.chepura@gmail.com>
2023-05-07 08:00:03 +0300

    dqn added angvel.y and distance from center (#71)

--- src/nn/params.rs
+++ src/nn/params.rs
@@ -3,16 +3,32 @@ use crate::car::SENSOR_COUNT;
 #[cfg(target_arch = "wasm32")]
 pub const EPOCHS: usize = 32;
 #[cfg(not(target_arch = "wasm32"))]
-pub const EPOCHS: usize = 512;
+pub const EPOCHS: usize = 100;
 
-pub const BATCH_SIZE: usize = 128;
+#[cfg(target_arch = "wasm32")]
+pub const BATCH_SIZE: usize = 32;
+#[cfg(not(target_arch = "wasm32"))]
+pub const BATCH_SIZE: usize = 64;
+
+#[cfg(target_arch = "wasm32")]
+pub const LEARNING_RATE: f32 = 0.01;
+#[cfg(not(target_arch = "wasm32"))]
+pub const LEARNING_RATE: f32 = 0.005;
+
+#[cfg(target_arch = "wasm32")]
 pub const STEP_DURATION: f64 = 1. / 10.;
-pub const DECAY: f32 = 0.001;
-pub const LEARNING_RATE: f32 = 0.002;
-pub const SYNC_INTERVAL_STEPS: usize = 100;
-pub const BUFFER_SIZE: usize = 10_000_000;
-pub const HIDDEN_SIZE: usize = 16;
+#[cfg(not(target_arch = "wasm32"))]
+pub const STEP_DURATION: f64 = 1. / 30.;
 
-pub const STATE_SIZE_BASE: usize = 3;
+#[cfg(target_arch = "wasm32")]
+pub const HIDDEN_SIZE: usize = 32;
+#[cfg(not(target_arch = "wasm32"))]
+pub const HIDDEN_SIZE: usize = 256;
+
+pub const DECAY: f32 = 0.001;
+pub const SYNC_INTERVAL_STEPS: usize = 300;
+pub const BUFFER_SIZE: usize = 10_000_000;
+
+pub const STATE_SIZE_BASE: usize = 5;
 pub const STATE_SIZE: usize = STATE_SIZE_BASE + SENSOR_COUNT;
 pub const ACTIONS: usize = 9; //
