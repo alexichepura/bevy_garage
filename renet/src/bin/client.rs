807f60a89aeace7ae615de132e5db9c1bbd28d17
Alexi Chepura <a.chepura@gmail.com>
2023-08-15 15:47:55 +0300

    copy example from renet

---
+++ renet/src/bin/client.rs
@@ -0,0 +1,321 @@
+use std::{collections::HashMap, net::UdpSocket, time::SystemTime};
+
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::{shape::Icosphere, *},
+    window::PrimaryWindow,
+};
+use bevy_egui::{EguiContexts, EguiPlugin};
+use bevy_garage_renet::{
+    connection_config, setup_level, ClientChannel, NetworkedEntities, PlayerCommand, PlayerInput,
+    ServerChannel, ServerMessages, PROTOCOL_ID,
+};
+use bevy_renet::{
+    renet::{
+        transport::{ClientAuthentication, NetcodeClientTransport, NetcodeTransportError},
+        RenetClient,
+    },
+    transport::NetcodeClientPlugin,
+    RenetClientPlugin,
+};
+use renet_visualizer::{RenetClientVisualizer, RenetVisualizerStyle};
+use smooth_bevy_cameras::{LookTransform, LookTransformBundle, LookTransformPlugin, Smoother};
+
+#[derive(Component)]
+struct ControlledPlayer;
+
+#[derive(Default, Resource)]
+struct NetworkMapping(HashMap<Entity, Entity>);
+
+#[derive(Debug)]
+struct PlayerInfo {
+    client_entity: Entity,
+    server_entity: Entity,
+}
+
+#[derive(Debug, Default, Resource)]
+struct ClientLobby {
+    players: HashMap<u64, PlayerInfo>,
+}
+
+fn new_renet_client() -> (RenetClient, NetcodeClientTransport) {
+    let client = RenetClient::new(connection_config());
+
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let current_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+    let client_id = current_time.as_millis() as u64;
+    let authentication = ClientAuthentication::Unsecure {
+        client_id,
+        protocol_id: PROTOCOL_ID,
+        server_addr,
+        user_data: None,
+    };
+
+    let transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
+
+    (client, transport)
+}
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    app.add_plugins(RenetClientPlugin);
+    app.add_plugins(NetcodeClientPlugin);
+    app.add_plugins(LookTransformPlugin);
+    app.add_plugins(FrameTimeDiagnosticsPlugin);
+    app.add_plugins(LogDiagnosticsPlugin::default());
+    app.add_plugins(EguiPlugin);
+
+    app.add_event::<PlayerCommand>();
+
+    app.insert_resource(ClientLobby::default());
+    app.insert_resource(PlayerInput::default());
+    let (client, transport) = new_renet_client();
+    app.insert_resource(client);
+    app.insert_resource(transport);
+
+    app.insert_resource(NetworkMapping::default());
+
+    app.add_systems(Update, (player_input, camera_follow, update_target_system));
+    app.add_systems(
+        Update,
+        (
+            client_send_input,
+            client_send_player_commands,
+            client_sync_players,
+        )
+            .run_if(bevy_renet::transport::client_connected()),
+    );
+
+    app.insert_resource(RenetClientVisualizer::<200>::new(
+        RenetVisualizerStyle::default(),
+    ));
+
+    app.add_systems(Startup, (setup_level, setup_camera, setup_target));
+    app.add_systems(Update, update_visulizer_system);
+    app.add_systems(Update, panic_on_error_system);
+
+    app.run();
+}
+
+// If any error is found we just panic
+fn panic_on_error_system(mut renet_error: EventReader<NetcodeTransportError>) {
+    for e in renet_error.iter() {
+        panic!("{}", e);
+    }
+}
+
+fn update_visulizer_system(
+    mut egui_contexts: EguiContexts,
+    mut visualizer: ResMut<RenetClientVisualizer<200>>,
+    client: Res<RenetClient>,
+    mut show_visualizer: Local<bool>,
+    keyboard_input: Res<Input<KeyCode>>,
+) {
+    visualizer.add_network_info(client.network_info());
+    if keyboard_input.just_pressed(KeyCode::F1) {
+        *show_visualizer = !*show_visualizer;
+    }
+    if *show_visualizer {
+        visualizer.show_window(egui_contexts.ctx_mut());
+    }
+}
+
+fn player_input(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_input: ResMut<PlayerInput>,
+    mouse_button_input: Res<Input<MouseButton>>,
+    target_query: Query<&Transform, With<Target>>,
+    mut player_commands: EventWriter<PlayerCommand>,
+) {
+    player_input.left = keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left);
+    player_input.right =
+        keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right);
+    player_input.up = keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up);
+    player_input.down = keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down);
+
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        let target_transform = target_query.single();
+        player_commands.send(PlayerCommand::BasicAttack {
+            cast_at: target_transform.translation,
+        });
+    }
+}
+
+fn client_send_input(player_input: Res<PlayerInput>, mut client: ResMut<RenetClient>) {
+    let input_message = bincode::serialize(&*player_input).unwrap();
+
+    client.send_message(ClientChannel::Input, input_message);
+}
+
+fn client_send_player_commands(
+    mut player_commands: EventReader<PlayerCommand>,
+    mut client: ResMut<RenetClient>,
+) {
+    for command in player_commands.iter() {
+        let command_message = bincode::serialize(command).unwrap();
+        client.send_message(ClientChannel::Command, command_message);
+    }
+}
+
+fn client_sync_players(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut client: ResMut<RenetClient>,
+    transport: Res<NetcodeClientTransport>,
+    mut lobby: ResMut<ClientLobby>,
+    mut network_mapping: ResMut<NetworkMapping>,
+) {
+    let client_id = transport.client_id();
+    while let Some(message) = client.receive_message(ServerChannel::ServerMessages) {
+        let server_message = bincode::deserialize(&message).unwrap();
+        match server_message {
+            ServerMessages::PlayerCreate {
+                id,
+                translation,
+                entity,
+            } => {
+                println!("Player {} connected.", id);
+                let mut client_entity = commands.spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Capsule::default())),
+                    material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                    transform: Transform::from_xyz(translation[0], translation[1], translation[2]),
+                    ..Default::default()
+                });
+
+                if client_id == id {
+                    client_entity.insert(ControlledPlayer);
+                }
+
+                let player_info = PlayerInfo {
+                    server_entity: entity,
+                    client_entity: client_entity.id(),
+                };
+                lobby.players.insert(id, player_info);
+                network_mapping.0.insert(entity, client_entity.id());
+            }
+            ServerMessages::PlayerRemove { id } => {
+                println!("Player {} disconnected.", id);
+                if let Some(PlayerInfo {
+                    server_entity,
+                    client_entity,
+                }) = lobby.players.remove(&id)
+                {
+                    commands.entity(client_entity).despawn();
+                    network_mapping.0.remove(&server_entity);
+                }
+            }
+            ServerMessages::SpawnProjectile {
+                entity,
+                translation,
+            } => {
+                let projectile_entity = commands.spawn(PbrBundle {
+                    mesh: meshes.add(
+                        Mesh::try_from(Icosphere {
+                            radius: 0.1,
+                            subdivisions: 5,
+                        })
+                        .unwrap(),
+                    ),
+                    material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                    transform: Transform::from_translation(translation.into()),
+                    ..Default::default()
+                });
+                network_mapping.0.insert(entity, projectile_entity.id());
+            }
+            ServerMessages::DespawnProjectile { entity } => {
+                if let Some(entity) = network_mapping.0.remove(&entity) {
+                    commands.entity(entity).despawn();
+                }
+            }
+        }
+    }
+
+    while let Some(message) = client.receive_message(ServerChannel::NetworkedEntities) {
+        let networked_entities: NetworkedEntities = bincode::deserialize(&message).unwrap();
+
+        for i in 0..networked_entities.entities.len() {
+            if let Some(entity) = network_mapping.0.get(&networked_entities.entities[i]) {
+                let translation = networked_entities.translations[i].into();
+                let transform = Transform {
+                    translation,
+                    ..Default::default()
+                };
+                commands.entity(*entity).insert(transform);
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+struct Target;
+
+fn update_target_system(
+    primary_window: Query<&Window, With<PrimaryWindow>>,
+    mut target_query: Query<&mut Transform, With<Target>>,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    let mut target_transform = target_query.single_mut();
+    if let Some(cursor_pos) = primary_window.single().cursor_position() {
+        if let Some(ray) = camera.viewport_to_world(camera_transform, cursor_pos) {
+            if let Some(distance) = ray.intersect_plane(Vec3::Y, Vec3::Y) {
+                target_transform.translation = ray.direction * distance + ray.origin;
+            }
+        }
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands
+        .spawn(LookTransformBundle {
+            transform: LookTransform {
+                eye: Vec3::new(0.0, 8., 2.5),
+                target: Vec3::new(0.0, 0.5, 0.0),
+                up: Vec3::Y,
+            },
+            smoother: Smoother::new(0.9),
+        })
+        .insert(Camera3dBundle {
+            transform: Transform::from_xyz(0., 8.0, 2.5)
+                .looking_at(Vec3::new(0.0, 0.5, 0.0), Vec3::Y),
+            ..default()
+        });
+}
+
+fn setup_target(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(
+                Mesh::try_from(Icosphere {
+                    radius: 0.1,
+                    subdivisions: 5,
+                })
+                .unwrap(),
+            ),
+            material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+            transform: Transform::from_xyz(0.0, 0., 0.0),
+            ..Default::default()
+        })
+        .insert(Target);
+}
+
+fn camera_follow(
+    mut camera_query: Query<&mut LookTransform, (With<Camera>, Without<ControlledPlayer>)>,
+    player_query: Query<&Transform, With<ControlledPlayer>>,
+) {
+    let mut cam_transform = camera_query.single_mut();
+    if let Ok(player_transform) = player_query.get_single() {
+        cam_transform.eye.x = player_transform.translation.x;
+        cam_transform.eye.z = player_transform.translation.z + 2.5;
+        cam_transform.target = player_transform.translation;
+    }
+}
